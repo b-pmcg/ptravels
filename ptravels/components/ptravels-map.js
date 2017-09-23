875549c3d871d179d607c255ba8b1735ea27bd7b
Phil Bain <bain.pmcg@gmail.com>
2017-09-23 17:15:27 -0600

    restructured promises in map component, split user and geo apis

--- ptravels/components/ptravels-map.js
+++ ptravels/components/ptravels-map.js
@@ -1,66 +1,102 @@
-import React, { Component } from 'react';
-import { Map, LayersControl, Marker, Popup, TileLayer, ZoomControl} from 'react-leaflet';
+import React, {Component} from 'react';
+import {
+    Map,
+    LayersControl,
+    Marker,
+    Popup,
+    TileLayer,
+    ZoomControl
+} from 'react-leaflet';
 import Control from 'react-leaflet-control';
 import NameForm from './name-form';
 import MarkerInfo from './marker-info';
-const { BaseLayer, Overlay } = LayersControl;
-
+const {BaseLayer, Overlay} = LayersControl;
+const utilities = require('../utilities');
 /*This is a container component that combines the
 Map component and single-input.js and is imported in app.js*/
 
 export default class PtravelsMap extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-          lat: 39.209425,
-          lng: -76.86181599999999,
-          zoom: 13,
-          message: "Enter your name to see your first show."
+            lat: 39.209425,
+            lng: -76.86181599999999,
+            zoom: 13,
+            message: "Enter your name to see your first show."
         };
-      }
+    }
 
     getNameValue = (nameValue) => {
         let self = this; // <-- How to avoid?
         fetch(`/usershows/${nameValue}`)
-          .then(response => {  
-          if (response.status !== 200) {  
-              console.log('Looks like there was a problem. Status Code: ' + response.status);  
-              return;  
-          }
-          response.json().then(data => {  
-              console.log(data); 
-              self.setState({lat: data.lat, lng: data.lng, message: nameValue});
-          })  
-          })
-          .then( () => {
-            //now fetch all venues
-          })
-          .catch(err => console.log('Fetch Error :-S', err)) //is this error handling implemented correctly?
+        .then(response => {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem with /usershows. Status Code: ' + response.status);
+                return;
+            }
+            return response.json(); 
+        })
+        .then(data => {
+            console.log(data);
+            var showString = utilities.getMostRecentShowString(data);
+            console.log(`showstring: ${showString}`)
+            return fetch(`/geodata/${showString}`)
+        })
+        .then(response => {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem with /geodata. Status Code: ' + response.status);
+                return;
+            }
+            return response.json(); 
+        })
+        .then(data => {
+            console.log("new data" + data);
+            var coords = utilities.getCoordsFromGeoData(data)
+            self.setState({lat: coords.lat, lng: coords.lng, message: nameValue});
+            //return fetch() //venue info
+        })
+        // .then(response => {
+        //     if (response.status !== 200) {
+        //         console.log('Looks like there was a problem with /venueinfo. Status Code: ' + response.status);
+        //         return;
+        //     }
+        //     return response.json(); 
+        // })
+        // .then(data => {
+        //     // handle venue info data here.
+        // })
+        .catch(err => console.log('Fetch Error: ', err)) //is this error handling implemented correctly?
+
     }
-  
+
     render() {
         const position = [this.state.lat, this.state.lng]
         const center = [51.505, -0.09];
-        const rectangle = [[51.49, -0.08], [51.5, -0.06]];
+        const rectangle = [
+            [
+                51.49, -0.08
+            ],
+            [51.5, -0.06]
+        ];
         const message = [this.state.message];
 
-    return (
-      <Map center={position} zoom={this.state.zoom}>
-      {/* <ZoomControl position="topright" /> */}
-      <TileLayer
-        url='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        attribution='© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        maxZoom={18}/>
-      
-      <Control position="topleft" >
-        <NameForm callbackFromParent={this.getNameValue}/>
-      </Control>
-      <Marker position={position}>
-          <Popup>
-            <MarkerInfo message={message}/>
-          </Popup>
-        </Marker>
-    </Map>
-    )
-  }
+        return (
+            <Map center={position} zoom={this.state.zoom}>
+                {/* <ZoomControl position="topright" /> */}
+                <TileLayer
+                    url='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    attribution='© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    maxZoom={18}/>
+
+                <Control position="topleft">
+                    <NameForm callbackFromParent={this.getNameValue}/>
+                </Control>
+                <Marker position={position}>
+                    <Popup>
+                        <MarkerInfo message={message}/>
+                    </Popup>
+                </Marker>
+            </Map>
+        )
+    }
 }
--- ptravels/server.js
+++ ptravels/server.js
@@ -10,9 +10,13 @@ const config = require('./webpack.config.babel.js');
 const compiler = webpack(config);
 const port = 3000;
 
-var http = require('http');
-var finalhandler = require('finalhandler');
-var Router = require('router');
+const http = require('http');
+const finalhandler = require('finalhandler');
+const Router = require('router');
+const headerSettings = {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Access-Control-Allow-Origin': 'http://localhost:3001'
+}
 
 const creds = require('./config');
 const M = creds.pnetConfig.methods;
@@ -32,56 +36,49 @@ function queryComposer(name, value) {
 }
 
 const queryToPass = [];
- 
+
 // Webpack
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
-    stats: {colors: true}
-  }));
+    stats: { colors: true }
+}));
 
 app.listen(port, () => {
     console.log(`Express server running on port: ${port}`);
-  });
-
-// Routes --Move this out!--  
-// app.get('/makerequest', async function (req, res) {
-// res.setHeader('Content-Type', 'application/json; charset=utf-8');
-// queryToPass.push(new queryComposer("showid", showid).queryString);
-// var pdata =  await gateway.testRequest(M.getAllVenues, queryToPass);
-// console.log(pdata);
-// res.write(JSON.stringify(pdata));
-// res.end();
-// });
-
-app.get('/usershows/:username', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-  let username = req.params.username;
-  let pnetData =  await gateway.getShowsByUsername(username);
-  let showString = utilities.getMostRecentShowString(pnetData);
-  let geoData = await gateway.getGeoData(showString);
-  if (!geoData) {
-    console.log("Handle this error better (server.js)");
-    return; // Clean this up somehow
-  }
-  let coords = utilities.getCoordsFromGeoData(geoData);
-  res.write(JSON.stringify(coords));
-  res.end();
 });
 
-app.get('/venueshows/:venue', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-  let venueName = req.params.venue;
-  let allVenueShows = gateway.getVenueByVenueId(venueName);
-  // get shows by venueid
+app.get('/usershows/:username', async(req, res) => {
+    res.set(headerSettings);
+    let username = req.params.username;
+    let pnetData = await gateway.getShowsByUsername(username);
+    console.log(pnetData);
+    res.write(JSON.stringify(pnetData));
+    res.end();
+});
 
-  res.write(JSON.stringify(coords));
-  res.end();
+app.get('/geodata/:showstring', async(req, res) => {
+    res.set(headerSettings);
+    let showString = req.params.showstring;
+    let geoData = await gateway.getGeoData(showString);
+    if (!geoData) {
+        console.log("Handle this error better (server.js)");
+        return; // Clean this up somehow
+    }
+    res.write(JSON.stringify(geoData));
+    res.end();
+})
+
+app.get('/venueshows/:venue', async(req, res) => {
+    res.set(headerSettings);
+    let venueName = req.params.venue;
+    let allVenueShows = gateway.getVenueByVenueId(venueName);
+    // get shows by venueid
+    res.write(JSON.stringify(coords));
+    res.end();
 
 })
-  
-  //END ROUTES
+
+//END ROUTES
 
 //gateway.makeRequest(M.getPeopleByShowId, queryToPass);
 
