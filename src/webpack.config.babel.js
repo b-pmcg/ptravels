4206623f75b754a4957712a3be73c42a83a9b6d1
Phil Bain <bain.pmcg@gmail.com>
2018-02-24 13:29:00 -0700

    more tests3

--- src/webpack.config.babel.js
+++ src/webpack.config.babel.js
@@ -22,7 +22,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].js',
-        publicPath: '/', //this is basically the webroot
+        publicPath: '/src', //this is basically the webroot
     },
     plugins: [
         new HtmlWebpackPlugin({
