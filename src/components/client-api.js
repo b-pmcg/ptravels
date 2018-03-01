0f29acb62109ce47ad2acf6ef13b09eb80fb8851
Phil Bain <bain.pmcg@gmail.com>
2018-02-28 20:50:42 -0700

    fetches from domain name

--- src/components/client-api.js
+++ src/components/client-api.js
@@ -1,7 +1,7 @@
 const utilities = require('../utilities');
 const rp = require('request-promise');
 const localHost = 'https://localhost:3000/';
-const webServer = 'https://159.65.75.193/';
+const webServer = 'https://phluffstravels.com/';
 const rootServer = '/';
 const server = webServer;
 /*This is supposed to be the workhorse to keep ptravels-map clean*/
